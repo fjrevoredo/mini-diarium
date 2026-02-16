0d0c61adb20a69f239dbc845c303f74b5f0be01a
Francisco J. Revoredo <39350477+fjrevoredo@users.noreply.github.com>
2026-02-17 00:46:15 +0100

    Feature v0.1.2 (#11)
    ## Summary
    
    Small fixes
    
    ## Changes
    
    - Change color of the main font in dark mode
    - Fix logging level
    - Fix simple architecture diagram

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -13,7 +13,10 @@ use tauri::Manager;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    env_logger::init();
+    env_logger::Builder::from_env(
+        env_logger::Env::default().default_filter_or("mini_diarium_lib=info"),
+    )
+    .init();
     info!("Mini Diarium starting");
 
     tauri::Builder::default()
